1de044753c69b66a93489b8df2c2a136aa2f9f6e
belxoch12 <beltranvilledaxochitlcitlally@gmail.com>
2023-05-29 10:34:06 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/blogapp_2practica/Register.kt
@@ -0,0 +1,11 @@
+package com.example.blogapp_2practica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
